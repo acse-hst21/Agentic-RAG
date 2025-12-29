ec5cdec42fa5ed04ce22c13ad489329b9ad50500
Hisham Taj <MrHishamTaj@gmail.com>
2025-12-29 11:27:38 +0000

    Complete CRAG implementation

--- graph/nodes/generate.py
+++ graph/nodes/generate.py
@@ -4,8 +4,8 @@ from graph.state import GraphState
 
 def generate(state: GraphState) -> [str, Any]:
     print("---GENERATE---")
-    question = state("question")
-    documents = state("documents")
+    question = state["question"]
+    documents = state["documents"]
 
     generation = generation_chain.invoke({"context": documents, "question": question})
     return {"documents": documents, "question": question, "generation": generation}
